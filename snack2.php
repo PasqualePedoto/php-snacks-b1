d150f769ac70a78e8e327d5106b4b4cb691173b5
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-30 16:58:17 +0200

    Snack 2 completato

--- snack2.php
+++ snack2.php
@@ -1,5 +1,34 @@
 <?php
-$prova = "prova";
+
+    // Controlliamo se vengono passati dei parametri, se si
+    // allora dichiariamo delle variabili PHP
+    if(!empty($_GET)){
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+        $age = $_GET['age'];
+        
+        // @  Metodo che ci ritorna la posizione, se esiste, dello snail
+        $snail_pos = strpos($mail,'@');
+
+        // @  Metodo che ci ritorna la posizione, se esiste, del punto
+        $point_pos = strpos($mail,'.');
+
+        // @  Funzione che consente di effettuare il type casting
+        $ctrl_age = intval($age);
+
+        // @  Variabile di controllo che determina l'accesso
+        $ctrl_ok = false;
+
+        // # Controlli innestati
+        if($snail_pos !== false && $point_pos !== false) {
+            if($ctrl_age !== 0){
+                if(mb_strlen($name) > 3){
+                    // * Settiamo il controllo
+                    $ctrl_ok = true;
+                }     
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +42,18 @@ $prova = "prova";
     </head>
 
     <body>
-    <p>
-        <?= $prova ?>
-    </p>
+        <!-- Accesso Consentito -->
+       <p>
+        <?php  if($ctrl_ok) { ?>
+            <p>Accesso consentito!</p>
+        <?php } ?>
+       </p>
+       <!-- Accesso negato -->
+       <p>
+       <?php  if(!$ctrl_ok) { ?>
+            <p>Accesso negato!</p>
+        <?php } ?>
+       </p>
     </body>
 
 </html>
