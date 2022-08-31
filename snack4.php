aab2feaf0fce3b70264eb94654cc6ee0fa07628a
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-31 12:40:01 +0200

    Snack 4 completato con una modifica

--- snack4.php
+++ snack4.php
@@ -7,9 +7,7 @@ $i = 0;
 while($i < $numbers_length){
     $number = rand(1,100);
 
-    if(strpos($random_number,$number) === false){
-        die();
-    }else {
+    if(strpos($random_number,$number) !== false){
         $random_number[] = $number;
         $i++;
     }
