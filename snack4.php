48073916132315b9d22e738308c5d90de8ea9ffa
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-31 12:36:49 +0200

    Snack 4 completato

---
+++ snack4.php
@@ -0,0 +1,20 @@
+<?php
+
+$random_number = [];
+$numbers_length = 15;
+$i = 0;
+
+while($i < $numbers_length){
+    $number = rand(1,100);
+
+    if(strpos($random_number,$number) === false){
+        die();
+    }else {
+        $random_number[] = $number;
+        $i++;
+    }
+}
+
+var_dump($random_number);
+
+?>
